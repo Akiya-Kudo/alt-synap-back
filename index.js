60512fca90d005e282fd49d2527ec6ed9af97a58
Akiya-Kudo <k.akiya07@gmail.com>
2022-10-21 23:59:11 +0900

    connect done

---
+++ index.js
@@ -0,0 +1,80 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = gql`
+    type Book {
+        title: String
+        author: Author
+    }
+    
+    type Author {
+        name: String
+        books: [Book]
+    }
+
+    type Query {
+        books: [Book]
+        authors: [Author]
+    }
+
+    type Mutation {
+        addBook(title: String, author: String): Book
+    }
+`;
+
+const books = [
+    {
+    title: 'The Awakening',
+    author: 'Kate Chopin',
+    },
+    {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+    },
+];
+const authors = [
+    {
+    name: 'Kate Chopin',
+    books: ["Kate Chopin", "City of Glass"],
+    },
+];
+
+
+// Resolvers define the technique for fetching the types defined in the
+// schema. This resolver retrieves books from the "books" array above.
+const resolvers = {
+    Query: {
+    books: () => books,
+    authors: () => author,
+    },
+};
+
+const {
+    ApolloServerPluginLandingPageLocalDefault
+} = require('apollo-server-core');
+
+  // The ApolloServer constructor requires two parameters: your schema
+  // definition and your set of resolvers.
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    csrfPrevention: true,
+    cache: 'bounded',
+    /**
+     * What's up with this embed: true option?
+     * These are our recommended settings for using AS;
+     * they aren't the defaults in AS3 for backwards-compatibility reasons but
+     * will be the defaults in AS4. For production environments, use
+     * ApolloServerPluginLandingPageProductionDefault instead.
+    **/
+    plugins: [
+    ApolloServerPluginLandingPageLocalDefault({ embed: true }),
+    ],
+});
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+});
