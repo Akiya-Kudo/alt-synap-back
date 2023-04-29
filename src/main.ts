a31af917ddfaacb152c7d006e593eef044e2bcf4
USER <user@USERnoMacBook-Air.local>
2023-04-30 02:36:15 +0900

    change schema

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaService } from './_prisma/prisma.service';
 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(4000);
