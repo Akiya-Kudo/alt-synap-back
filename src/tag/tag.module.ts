aff4a33fb13f9a7822ecec98847c0ffefb1327e2
USER <user@USERnoMacBook-Air.local>
2023-10-03 20:20:45 +0900

    made hot topic query

--- src/tag/tag.module.ts
+++ src/tag/tag.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TagResolver } from './tag.resolver';
 import { TagService } from './tag.service';
 import { PrismaModule } from 'src/_prisma/prisma.module';
+import { RedisModule } from 'src/_redis/redis.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, RedisModule],
   providers: [TagResolver, TagService],
   exports: [TagResolver, TagService],
 })
