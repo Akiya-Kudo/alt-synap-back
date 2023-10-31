5d71103a0ecd4e7a48511bb48b9ac51c99315cc6
USER <user@USERnoMacBook-Air-2.local>
2023-10-31 14:06:35 +0900

    change cors middleware add option case process

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaService } from './_prisma/prisma.service';
-import * as serviceAccount from '../tipsy-firebase.json'
+// import * as serviceAccount from '../tipsy-firebase.json'
 import admin from "firebase-admin";
 
 async function bootstrap() {
   admin.initializeApp({
-    // credential: admin.credential.cert({
-    //   "type": process.env.TYPE,
-    //   "project_id": process.env.PROJECT_ID,
-    //   "private_key_id": process.env.PRIVATE_KEY_ID,
-    //   "private_key": process.env.PRIVATEKEY,
-    //   "client_email": process.env.CLIENT_EMAIL,
-    //   "client_id": process.env.CLIENT_ID,
-    //   "auth_uri": process.env.AUTH_URL,
-    //   "token_uri": process.env.TOKEN_URL,
-    //   "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_X509_CERT_URL,
-    //   "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL,
-    //   "universe_domain": process.env.UNIVERSE_DOMAIN
-    // } as admin.ServiceAccount)
-    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount)
+    credential: admin.credential.cert({
+      "type": process.env.TYPE,
+      "project_id": process.env.PROJECT_ID,
+      "private_key_id": process.env.PRIVATE_KEY_ID,
+      "private_key": process.env.PRIVATEKEY,
+      "client_email": process.env.CLIENT_EMAIL,
+      "client_id": process.env.CLIENT_ID,
+      "auth_uri": process.env.AUTH_URL,
+      "token_uri": process.env.TOKEN_URL,
+      "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_X509_CERT_URL,
+      "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL,
+      "universe_domain": process.env.UNIVERSE_DOMAIN
+    } as admin.ServiceAccount)
+    // credential: admin.credential.cert(serviceAccount as admin.ServiceAccount)
   })
   const app = await NestFactory.create(AppModule);
   await app.listen(4000);
